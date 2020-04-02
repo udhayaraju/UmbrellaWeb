d20fad17f0a8e85cf40d8ee8a7af498b11db572e
udhaya raju <udhaya.raju@hotmail.com>
2020-04-02 12:54:40 +0100

    Add project files.

---
+++ UmbrellaWeb/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UmbrellaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    
+        public ActionResult Contractors()
+        {
+            base.ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Referafriend()
+        {
+            base.ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            base.ViewBag.Message = "Your contact page.";
+            return View();
+        }
+       
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+   
+    }
+}
